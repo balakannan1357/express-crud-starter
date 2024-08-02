5424a8039edc28c508a9690ec325db0f8f7044cd
Balakannan <balakannan2522@gmail.com>
2024-08-02 06:56:57 +0530

    update: mongoose package & middleware

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,19 +1,26 @@
+import { HttpException } from '@exceptions/httpException';
 import { plainToInstance } from 'class-transformer';
 import { validateOrReject, ValidationError } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
-import { HttpException } from '@exceptions/httpException';
 
 /**
  * @name ValidationMiddleware
  * @description Allows use of decorator and non-decorator based validation
  * @param type dto
+ * @param value Where to find the object to validate
  * @param skipMissingProperties When skipping missing properties
  * @param whitelist Even if your object is an instance of a validation class it can contain additional properties that are not defined
  * @param forbidNonWhitelisted If you would rather to have an error thrown when any non-whitelisted properties are present
  */
-export const ValidationMiddleware = (type: any, skipMissingProperties = false, whitelist = false, forbidNonWhitelisted = false) => {
+export const ValidationMiddleware = (
+  type: any,
+  value: 'body' | 'query' | 'params' = 'body',
+  skipMissingProperties = false,
+  whitelist = false,
+  forbidNonWhitelisted = false,
+) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const dto = plainToInstance(type, req.body);
+    const dto = plainToInstance(type, req[value]);
     validateOrReject(dto, { skipMissingProperties, whitelist, forbidNonWhitelisted })
       .then(() => {
         req.body = dto;
